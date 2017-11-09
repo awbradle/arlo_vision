bb2753269e405c3b6bda864eca91044284d2bbe9
Andrew Bradley <bradleyandrew13@gmail.com>
2017-11-09 09:52:29 -0500

    work for nov 9th

--- python/PCReadArrowKeysAbridged.py
+++ python/PCReadArrowKeysAbridged.py
@@ -27,7 +27,9 @@ NEGATIVE_VALUE=chr(0)
 COMMAND=chr(0)
 NUMBER=chr(0)
 
-
+ker3 = np.ones((3,3))/9.0
+ker5 = np.ones((5,5))/25.0
+ker9 = np.ones((9,9))/81.0
 
 
 
@@ -36,20 +38,20 @@ NUMBER=chr(0)
 
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-	port='COM9',
-	baudrate=9600,
-	parity=serial.PARITY_ODD,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS
-)
-
-
-if (ser.isOpen()):
-	ser.close()
-
-
-ser.open()
+#ser = serial.Serial(
+#	port='COM9',
+#	baudrate=9600,
+#	parity=serial.PARITY_ODD,
+#	stopbits=serial.STOPBITS_ONE,
+#	bytesize=serial.EIGHTBITS
+#)
+#
+#
+#if (ser.isOpen()):
+#	ser.close()
+#
+#
+#ser.open()
 
 while 1:
 
@@ -58,9 +60,19 @@ while 1:
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    gray9 = cv2.filter2D(gray,-1,ker3)
+    gray25 = cv2.filter2D(gray,-1,ker5)
+    gray81= cv2.filter2D(gray,-1,ker9)
+    
+    canny = cv2.Canny(gray9, 100,200)
+    canny3 = cv2.Canny(gray9, 50,200)
+    canny5 = cv2.Canny(gray9, 100,250)
+    canny9 = cv2.Canny(gray9, 50,250)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',canny)
+    cv2.imshow('gray9',canny3)
+    cv2.imshow('gray25',canny5)
+    cv2.imshow('gray81',canny9)
     key = cv2.waitKey(1)
    
     if(key == 112):
